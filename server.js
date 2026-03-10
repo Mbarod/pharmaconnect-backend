97859669e0d429e8431dd10b190a0d61aece9f90
MBA AOUDOU rodney <mbarodney56@gmail.com>
2026-03-11 00:55:26 +0100

    fix search results fields

--- server.js
+++ server.js
@@ -177,8 +177,8 @@ results.push({
   pharmacy_latitude: pharmacy.latitude,
   pharmacy_longitude: pharmacy.longitude,
 
-  opening time: pharmacy.opening time,
-  closing time: pharmacy.closing time,
+  opening_time: pharmacy.opening_time,
+  closing_time: pharmacy.closing_time,
 
   status_open: status_open,
 
